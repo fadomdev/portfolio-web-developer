18c67e39630953b7c483aa90adae93f318aee42a
Francisco Aquino <franciscoaquino19@gmail.com>
2024-06-25 02:00:30 -0400

    chore: Update npm dependency to latest stable version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
+// Import /serverless for a Serverless SSR site
+import vercelServerless from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercelServerless({
+    webAnalytics: {
+      enabled: true
+    },
+    maxDuration: 8
+  }),
   integrations: [tailwind(), react()]
 })
