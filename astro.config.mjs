7d4502c0f40bb9def244905ba80b9a6a4d324966
Francisco <franciscoaquino19@gmail.com>
2024-09-03 20:36:04 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import react from '@astrojs/react'
 import db from '@astrojs/db'
 
 // Import /serverless for a Serverless SSR site
-import vercel from '@astrojs/vercel'
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react(), db()],
-  adapter: vercel()
+  adapter: vercelServerless()
 })
